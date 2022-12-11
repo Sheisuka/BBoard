4a4c5a79a2c54370ce9c19e7232e77b4dea7e607
sheisuka <xxtornex@gmail.com>
2022-12-11 23:13:15 +0700

    feature: add login

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, other_page
+from .views import index, other_page, BBLoginView
 
 
 urlpatterns = [
     path('<str:page>/', other_page, name='other_page'),
     path('', index, name='index'),
+    path('accounts/login/', BBLoginView.as_view(), name='login')
 ]
