118ea191f2702aebbd74d48c286e4ef48d79a019
sheisuka <xxtornex@gmail.com>
2022-12-15 00:13:24 +0700

    feature: add registraion

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+
+from .apps import user_registered
 
 from .models import AdvUser
 
@@ -9,3 +13,38 @@ class ChangeUserInfoForm(forms.ModelForm):
     class Meta:
         model = AdvUser
         fields = ('username', 'email', 'first_name', 'last_name', 'send_messages')
+
+
+class RegisterUserForm(forms.ModelForm):
+    email = forms.EmailField(required=True, label='Адрес электронной почты')
+
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput,
+                                help_text=password_validation.password_validators_help_text_html())
+    password2 = forms.CharField(label='Пароль (повторно)', widget=forms.PasswordInput,
+                                help_text='Введите тот же самый пароль для проверки')
+
+
+    def clean_password1(self):
+        super().clean()
+        password1 = self.cleaned_data['password1']
+        password2 = self.changed_data['password2']
+        if password1 != password2 and password2 and password1:
+            errors = {'password2': ValidationError('Введенные пароли не совпадают',
+                                    code='password_mismatch')}
+            raise ValidationError(errors)
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = False
+        user.is_activated = False
+        if commit:
+            user.save()
+        user_registered.send(RegisterUserForm, instance=user)
+        return user
+
+
+    class Meta:
+        model = AdvUser
+        fields = ('username', 'email', 'password1', 'password2', 'first_name',
+                    'last_name', 'send_message')
