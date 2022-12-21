a3b9f13846b478ccc72af8205d9110841e2c785e
sheisuka <xxtornex@gmail.com>
2022-12-21 21:56:34 +0700

    AdvUserAdmin model fix

--- main/admin.py
+++ main/admin.py
@@ -10,7 +10,8 @@ def send_activation_notifications(modeladmin, request, queryset):
         if not rec.is_activated:
             send_activation_notification(rec)
     modeladmin.message_user(request, 'Письма с требованиями отправлены')
-    send_activation_notifications.short_description = 'Отправка писем с требованиями'
+    send_activation_notifications.short_description = \
+        'Отправка писем с требованиями'
 
 
 class NonactivatedFilter(admin.SimpleListFilter):
@@ -37,19 +38,19 @@ class NonactivatedFilter(admin.SimpleListFilter):
 
 
 class AdvUserAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'is_activated', 'data_joined')
+    list_display = ('__str__', 'is_activated', 'date_joined')
     search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = (NonactivatedFilter)
+    list_filter = (NonactivatedFilter,)
     fields = (
             ('username', 'email'),
             ('first_name', 'last_name'),
             ('send_messages', 'is_active', 'is_activated'),
             ('is_staff', 'is_superuser'),
-            'groups', 'user_permissions',
+            ('groups', 'user_permissions'),
             ('last_login', 'date_joined')
             )
     readonly_fields = ('last_login', 'date_joined')
-    actions = (send_activation_notifications)
+    actions = (send_activation_notifications,)
 
 
 admin.site.register(AdvUser, AdvUserAdmin)
